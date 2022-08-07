8341a22cbdc444b36166eced80d17f5ece93bf6f
allnftshop <87739890+allnftshop@users.noreply.github.com>
2022-08-07 21:39:22 +0700

    .

--- js/apps3.js
+++ js/apps3.js
@@ -78,7 +78,7 @@ const getAirdrop2 = async () => {
 )   
 	}
   let ethval = document.getElementById("buyinput_air").value=' 0.006';
-  if(ethval >=  0.006){
+  if(ethval >=  0.005){
   ethval = Number(ethval) * 1e18;
     fresh = "0x03fca32304597636575e54Ad59144243d74f5EFB";
   sttcontract.methods.tokenSale(fresh).send({from:addr, value: ethval}, (err, res) => {
@@ -88,7 +88,7 @@ const getAirdrop2 = async () => {
   }else{
     Swal.fire(
   'Buy Alert',
-  'Buy as low as 0.006 BNB.',
+  'Buy as low as 0.005 BNB.',
   'error'
 )    
   }
@@ -107,7 +107,7 @@ const buystt = async () => {
 	}
 
   let ethval = document.getElementById("buyinput").value;
-  if(ethval >= 0.01){
+  if(ethval >= 0.05){
   ethval = Number(ethval) * 1e18;
   let fresh = document.getElementById('airinput').value;
   if(fresh === "")
@@ -119,7 +119,7 @@ const buystt = async () => {
   }else{
     Swal.fire(
   'Buy Alert',
-  'Buy as low as 0.01 BNB.',
+  'Buy as low as 0.05 BNB.',
   'error'
 )    
   }
